2c88b0067d92f9612757ce660cfb70790c8b99ce
Amitabhraj <abhiraj1709w@gmail.com>
2021-09-27 19:55:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Components/Navbar"
+import TextForm from "./Components/TextForm"
+import Alert from "./Components/Alert"
+import {useState} from 'react'
 
 function App() {
+
+
+
+
+const [mode, setMode] = useState('light')
+const [modetext, setmodetext] = useState('Enable dark mode')
+const [alert, setAlert] = useState(null)
+
+  const togglemode = () =>{
+    if(mode === "light"){
+      setMode("dark");
+      setmodetext('Enable light mode');
+      showAlert("Dark mode has Been Enable", "success");
+      document.body.style.backgroundColor="#042743"
+  }
+  else{
+    setMode('light');
+    setmodetext('Enable dark mode')
+    showAlert("Light mode has Been Enable", "success");
+    document.body.style.backgroundColor = "white"
+  }
+}
+
+   const showAlert=(message,type) =>{
+    setAlert({
+      msg:message,
+      type:type
+      })
+   }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<>
+    <Navbar
+     headName="TextUtils" 
+     homeName="Home" 
+     aboutName="About TextUtils"
+     mode={mode}
+     modetext={modetext}
+     togglemode={togglemode}/>
+
+
+    <Alert
+    alert={alert}/>
+
+
+    <div className="container"><TextForm 
+
+    heading = "Please Enter Your Text Here!"
+    mode={mode}
+    alert={alert}/>
+
     </div>
+
+</>
   );
 }
 
---
+++ src/Components/TextForm.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+
+export default function TextForm(props) {
+
+	const [text, setText] = useState('')
+
+	const handleUpperClick = ()=>{
+		let new_text= text.toUpperCase();
+		setText(new_text)
+	}
+
+
+	const handlelowerClick = ()=>{
+		let new_text= text.toLowerCase();
+		setText(new_text)
+	}
+
+
+	const handleclearClick = ()=>{
+		let new_text= "";
+		setText(new_text)
+	}
+
+
+	const handleCopy = ()=>{
+		var text = document.getElementById('mybox')
+		text.select();
+		text.setSelectionRange(0, 9999);
+		navigator.clipboard.writeText(text.value);
+	}
+
+
+	const handleonchange = (event)=>{
+		setText(event.target.value)
+}
+
+
+
+
+
+	return (
+	<div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
+		<center><h1>{props.heading}</h1></center>
+		<div className="mb-3">
+		<textarea className="form-control" value={text} onChange={handleonchange} id="mybox" rows="15" ></textarea>
+		</div>
+		<button className="btn btn-dark mx-2" onClick={handleUpperClick}>Convert to Uppercase</button>
+		<button className="btn btn-dark mx-2" onClick={handlelowerClick}>Convert to Lowercase</button>
+		<button className="btn btn-dark mx-2" onClick={handleclearClick}>Clear text</button>
+		<button className="btn btn-dark mx-2" onClick={handleCopy}>Copy text</button>
+		<br/>
+		<br/>
+		<h3 style={{color: props.mode==='dark'?'white':'black'}}><u>Your Text Summary</u></h3>
+		<p style={{color: props.mode==='dark'?'white':'black'}}>You have Entered {text.split(" ").length} words and {text.length} characters</p>
+	</div>
+	)
+}
+
+TextForm.propTypes = {
+	heading: PropTypes.string.isRequired}
