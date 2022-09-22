9142859da949989200d90560893f3d279dc8db83
Amitabh Raj <83171896+Amitabhraj@users.noreply.github.com>
2022-09-22 16:12:18 +0530

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -20,27 +20,13 @@ return (
             <ul className="list-unstyled" id="ul-contact">
                 <li className="mx-5 mt-5">
                 	<Si.SiGmail id="Gmail"/><br/><br/>
-                	<h3>amitabhraj07@gmail.com</h3>
-                </li>
-
-                <li className="mx-5 mt-5">
-                	<FaIcons.FaLinkedin id="linkedin"/><br/><br/>
-                	<a href="https://www.linkedin.com/in/amitabhraj/" id="link_contact">
-                		<button className="btn btn-dark">linkedin Profile</button>
-                	</a>
-                </li>
-
-                <li className="mx-5 mt-5">
-                	<FaIcons.FaGithub id="github"/><br/><br/>
-                	<a href="https://github.com/Amitabhraj" id="link_contact">
-                		<button className="btn btn-dark">My Github Profile</button>
-                	</a>
+                	<h3 id="phone_color">geniusraunakmishra@gmail.com</h3>
                 </li>
 
 
                 <li className="mx-5 mt-5">
                     <FaIcons.FaPhone id="phone"/><br/><br/>
-                        <h3>(+91)8409913276</h3>
+                        <h3 id="phone_color">+91 9798140938</h3>
                 </li>
 
 
