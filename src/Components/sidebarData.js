aa3063e1dd6e169524a5f6811cf2820759e22ecd
Amitabh Raj <83171896+Amitabhraj@users.noreply.github.com>
2022-09-22 16:14:20 +0530

    Update sidebarData.js

--- src/Components/sidebarData.js
+++ src/Components/sidebarData.js
@@ -13,29 +13,11 @@ export const SidebarData = [
     cName: 'nav-text'
   },
   {
-    title: 'Skills',
-    path: '/skill',
-    icon: <IoIcons.IoIosBulb />,
-    cName: 'nav-text'
-  },
-  {
-    title: 'Experience',
-    path: '/experience',
-    icon: <Ri.RiUserStarLine />,
-    cName: 'nav-text'
-  },
-  {
     title: 'Contact Me',
     path: '/contactme',
     icon: <FaIcons.FaPhone />,
     cName: 'nav-text'
-  },
-  {
-    title: 'Resume',
-    path: '/resume',
-    icon: <FaIcons.FaFile />,
-    cName: 'nav-text'
-  },
+  }
 ];
 
 
